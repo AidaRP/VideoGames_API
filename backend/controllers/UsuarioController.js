e7613c67f8e9c78b94569c54b6f2abbbd3bcab66
AidaRP <aminingcrypto@gmail.com>
2021-11-20 00:31:21 +0100

    última modificación de los end-point

--- backend/controllers/UsuarioController.js
+++ backend/controllers/UsuarioController.js
@@ -9,7 +9,8 @@ const UsuarioController = {}; //Create the object controller
 //Login usuario with database
 //get usuario
 UsuarioController.signIn = (req, res) =>{
-        let { correo, clave } = req.body;
+        let correo = req.body.correo;
+        let clave  = req.body.clave;
         // Buscar usuario
         usuario.findOne({ where: { correo: correo }
         }).then(usuario => {
@@ -41,15 +42,15 @@ UsuarioController.signIn = (req, res) =>{
 //REGISTER new usuario in database
 //create usuario
 UsuarioController.signUp = (req, res)=> {
-
+if (req.user.usuario.rol == "administrador") {
         // Encriptamos la contraseña
         let clave = bcrypt.hashSync(req.body.clave, Number.parseInt(authConfig.rounds));
-
+if (clave.lenght >= 8){
         // Crear un usuario
         usuario.create({
             nombre: req.body.nombre,
             correo: req.body.correo,
-            clave: clave
+            
         }).then(usuario => {
 
             // Creamos el token
@@ -65,8 +66,141 @@ UsuarioController.signUp = (req, res)=> {
         }).catch(err => {
             res.status(500).json(err);
         });
-
+    } else{
+        res.send({ 
+            message: `La contraseña tiene que tener un mínimo de 8 caracteres.${clave}`
+        });
+        };
+    }else{
+        res.send({
+            message:`Careces de permiso para el registro de usuarios, ponte en contacto con un administrador. Gracias.`
+        })
     };
+};
+
+//LISTADO DE TODOS LOS USUARIOS
+
+UsuarioController.getAll = (req,res) => {
+    if (req.user.usuario.rol == "administrador") {
+
+        usuario.findAll()
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message:
+                err.message || "Error detectado al intentar acceder a los usuarios."
+            });
+        });
+    }else{
+        res.send({
+            message: `No tienes permisos para visualizar a todos los usuarios. Contacto con un administrador`
+        });
+    }
+};
+
+
+//Busqueda de usuario por Id
+UsuarioController.getById = (req, res) => {
+    const id = req.params.id;
+
+    usuario.findByPk(id)
+    .then(data => {
+      if (data) {
+        res.send(data);
+      } else {
+        res.status(404).send({
+          message: `Cannot find usuario with id=${id}.`
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Error retrieving categories with id=" + id
+      });
+    });
+};
+
+
+//-------------------------------------------------------------------------------------
+
+UsuarioController.update = (req, res) => {
+
+      const id = req.params.id;
+
+      if (req.user.usuario.rol == "administrador" || req.user.usuario.id == id) {// HACEMOS QUE SOLO PUEDA ACTULIZARLO EL ADMINISTRADOR O EL USUARIO DUEÑO DEL PERFIL
+
+            
+          
+            usuario.update(req.body, {
+              where: { id: id }
+            })
+              .then(num => {
+                if (num == 1) {
+                  res.send({
+                    message: "El usuario ha sido actualizado correctamente."
+                  });
+                } else {
+                  res.send({
+                    message: `No se ha podido actualizar el usuario con el id ${id}`
+                  });
+                }
+              })
+              .catch(err => {
+                res.status(500).send({
+                  message: "Ha surgido algún error al intentar actualizar el usuario con el id " + id + "."
+                });
+              });
+      }else{
+        res.send({
+          message: `No tienes permisos para modificar el perfil indicado.`
+        });
+      }
+};
+
+//borrar todos los usuarios por id
+UsuarioController.delete = (req, res) => {
+  const id = req.params.id;
+
+  usuario.destroy({
+    where: { id: id }
+  })
+    .then(num => {
+      if (num == 1) {
+        res.send({
+          message: "usuario was deleted successfully!"
+        });
+      } else {
+        res.send({
+          message: `Cannot delete usuario with id=${id}. Maybe Movie was not found!`
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Could not delete usuario with id=" + id
+      });
+    });
+};
+//borrar todos los usuarios
+UsuarioController.deleteAll = (req, res) => {
+  usuarios.destroy({
+    where: {},
+    truncate: false
+  })
+    .then(nums => {
+      res.send({ message: `${nums} categories were deleted successfully!` });
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while removing all categories."
+      });
+    });
+};
 
 module.exports = UsuarioController;
-
