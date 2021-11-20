b6b62133c98b45fa342d6870b9c0f3cf5c01d59b
AidaRP <90700748+AidaRP@users.noreply.github.com>
2021-11-20 16:39:58 +0100

    Update auth.js

--- backend/config/auth.js
+++ backend/config/auth.js
@@ -1,5 +1,5 @@
 module.exports = {
     secret: process.env.AUTH_SECRET || "$sbTbAmRtu89", //Clave usada para encriptar
-    expires: process.env.AUTH_EXPIRES || "24h",      //Duracion del token
+    expires: process.env.AUTH_EXPIRES || "24h",      //Duracion del token 
     rounds: process.env.AUTH_ROUNDS || 10            //Numero de encriptaciones de contraseña
 }
